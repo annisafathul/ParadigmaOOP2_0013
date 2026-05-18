a5175f04d29b5646e531b2e9cbf06b15ca818bc3
annisafathul <annisafathul18@gmail.com>
2026-05-18 11:10:03 +0700

    membuat polimorpi class derived dari class baseClass

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -9,3 +9,12 @@ class baseClass
         cout << "Hallo saya Function dari base class";
     }
 };
+
+class derivedClass : public baseClass 
+{
+    public:
+    void perkenalan1() 
+    {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
